47038519795bf2576c373f6a0cab545f82671637
Jongsu Lee <jongsu_lee@me.com>
2021-06-28 16:51:17 +0900

    [python]problem note

---
+++ Python/Problem/mid_text.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+def gen_rand_text(num):
+  letters = string.ascii_lowercase
+  return ''.join(random.choice(letters) for i in range(num))
+
+def get_mid(s):
+    return "change of problem with anwser needed"
+
+print(get_mid(gen_rand_text(10)))
+print(get_mid(gen_rand_text(9)))
+print(get_mid(gen_rand_text(8)))
+print(get_mid(gen_rand_text(7)))
+print(get_mid(gen_rand_text(100)))
+
+
+
